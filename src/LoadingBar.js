304fac3f22e9933307d95ee7ad0784853963bf8b
bibi <me@pannnda.com>
2019-03-13 01:52:35 +0800

    createProtal

--- src/LoadingBar.js
+++ src/LoadingBar.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import ReactDOM from 'react-dom'
 import { LoadingBarProvider } from './Context'
 import './index.scss'
 
@@ -6,10 +7,27 @@ export const LoadingBar = (props) => {
   
   return (
     <LoadingBarProvider>
-      <div>
-        loading...
-      </div>
+      <Portal>
+        <div>
+          loading...
+        </div>
+      </Portal>
       {props.children}
     </LoadingBarProvider>
+  )
+}
+
+const Portal = (props) => {
+  const el = document.createElement('div')
+  useEffect(() => {
+    document.body.appendChild(el)
+    return () => {
+      document.body.removeChild(el)
+    }
+  },[])
+
+  return ReactDOM.createPortal(
+    props.children,
+    el
   )
 }
